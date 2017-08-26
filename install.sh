5697d58c342a2aa3aa849a6e55fe4816e8cb2371
ksomemo <kntsmy@hotmail.co.jp>
2017-08-26 16:55:14 +0900

    Edit install(settings of symblic links)

--- install.sh
+++ install.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
 
+install() {
+    local files=(
+     .gitconfig
+     .gitignore_global
+     .kerlrc
+     .tmux.conf
+     .vim
+     .vimrc
+     .zshrc
+    )
+    local script_dir_path=$(dirname $(readlink -f $0))
+    echo $script_dir_path
+    for f in ${files[@]}
+    do
+        echo $script_dir_path/$f to $HOME/$f
+        ln -s $script_dir_path/$f $HOME/$f
+    done
+}
+
+install
